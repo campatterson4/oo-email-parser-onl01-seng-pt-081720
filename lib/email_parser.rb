30a549df77bcb45ff5b5f0c33daf79e5f64cdbbf
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-23 22:22:24 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,21 +1,14 @@
-class EmailParser
+class EmailAddressParser
 
-  attr_accessor :emails
+    attr_accessor :email
+    def  initialize(emails)
+      @email = emails
+    end
 
-  def initialize(emails)
-    @emails = emails
+    def parse
+      email_array = @email.split(/[, ]/).uniq
+      email_array.reject! {|element| element.empty?}
+      email_array
+    end
+
   end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
-
-end
-
-emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
-parser = EmailParser.new(emails)
-
-puts parser.parse
