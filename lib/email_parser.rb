2c0254ccf0377df81ee7372b6383a7a52d6d1f81
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-23 22:20:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,22 +1,14 @@
 class EmailParser
-attr_accessor :emails 
-
-def initialize(emails)
-  @emails = emails
-end 
-
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
+	attr_accessor :emails
+	
+	def initialize(emails)
+		@emails= emails
+	end
+	
+	def parse
+		@emails = @emails.gsub(/[\s,]/ ," ").split
+		# @email is an array now
+		@emails.uniq
+	end
+	
 end
-
-end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
